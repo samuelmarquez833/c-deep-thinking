1ce72ffb1d74726f915e7e9dde939592ea3f97cd
Samuel Marquez <sammarquez833@outlook.com>
2026-01-07 16:15:33 -0600

    basic graph implementation

--- data_structures/grafos/graphs.c
+++ data_structures/grafos/graphs.c
@@ -1,45 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    int n;
-    int **adj;
-} Graph;
 
-Graph* graph_create(int n) {
-    Graph *g = malloc(sizeof(Graph));
-    if (!g) return NULL;
+/*
+asi como enl la lista enlazada no solo tienes a los nodos, sino que tiene s variables HEAD y TAIL
+aqui necesitas una estructura de grafo aparte de los nodos
 
-    g->n = n;
-    g->adj = malloc(n * sizeof(int*));
-    if (!g->adj) { free(g); return NULL; }
+*/
 
-    for (int i = 0; i < n; i++) {
-        g->adj[i] = calloc(n, sizeof(int)); // todo en 0
-        if (!g->adj[i]) {
-            // rollback
-            for (int k = 0; k < i; k++) free(g->adj[k]);
-            free(g->adj);
-            free(g);
-            return NULL;
-        }
+struct Node {
+    int value;
+    struct Node **apunta;
+    int lenghtOfApunta;
+};
+
+// usualmentelas demas cosas que se aniaden al la definicon del grafo con conveniencia
+// lo que defini aqui abajo es lo basico minimo, pero no estandar
+struct Graph {
+    int amountOfNodes;
+    struct Node *begins;
+};
+
+
+
+struct Node *createNode(int val){
+    struct Node *node = malloc(sizeof(struct Node ));
+    if (node == NULL){
+        return NULL;
     }
-    return g;
+
+    node->value = val;
+    node->apunta = NULL;
+    node->lenghtOfApunta = 0;
+    return node;
+
 }
 
-void graph_add_edge(Graph *g, int u, int v) {
-    // dirigido: u -> v
-    g->adj[u][v] = 1;
+void makeConnection(struct Node *nodeA, struct Node *nodeB){
+    
+    nodeA->lenghtOfApunta++;
+
+
+    if (nodeA->apunta == NULL){
+        nodeA->apunta = malloc((nodeA->lenghtOfApunta) * sizeof(struct Node*));
+    } else {
+        nodeA->apunta = realloc(nodeA->apunta, (nodeA->lenghtOfApunta) * sizeof(struct Node*));
+    }
+
+    
+    // intentar poner el nodo a lo ultimo 
+    
+    // ya creci el largo arriba
+    // quiero meter la direccion del nodo (que claro es tipo struct Node) 
+    // al final del vector, pero quiero hacerlo con artimetica de puntos
+    // explica no des codigo
+    *(nodeA->apunta+nodeA->lenghtOfApunta) = nodeB;
 }
 
-void graph_add_undirected_edge(Graph *g, int u, int v) {
-    g->adj[u][v] = 1;
-    g->adj[v][u] = 1;
+
+int main (){
+
+    struct Graph *grafo = malloc(sizeof(struct Graph));
+    struct Node *nodeA = createNode(5);
+    grafo->begins = nodeA;
+    struct Node *nodeB = createNode(6);
+    struct Node *nodeC = createNode(7);
+    makeConnection(nodeA, nodeB);
+    makeConnection(nodeA, nodeC);
+
+
+    int largo = nodeA->lenghtOfApunta;
+    printf("%d\n", largo);
+
+
+    struct Node *punteroActual;
+    for (int i = 0; i <= largo; i++){
+        punteroActual = *(nodeA->apunta+i);
+        printf("%p\n", punteroActual);
+    }
+
+    printf("porno\n");
+
+    //OJO recuerda el -> ya hace un primer *
+
+    printf("%p\n", nodeA->apunta);
+    printf("%p\n", nodeB);
+    printf("%p\n", nodeC);
+
+
 }
 
-void graph_free(Graph *g) {
-    if (!g) return;
-    for (int i = 0; i < g->n; i++) free(g->adj[i]);
-    free(g->adj);
-    free(g);
-}
+
