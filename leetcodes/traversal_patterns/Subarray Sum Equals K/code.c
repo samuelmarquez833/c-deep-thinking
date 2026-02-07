79968edd9257dd92b9fcfd97bd3810f312a227f4
Samuel Marquez <sammarquez833@outlook.com>
2026-02-07 14:22:55 -0600

    donde with prefix sum

--- leetcodes/traversal_patterns/Subarray Sum Equals K/code.c
+++ leetcodes/traversal_patterns/Subarray Sum Equals K/code.c
@@ -1,85 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
+typedef struct Node {
+  int key, val;
+  struct Node* next;
+} Node;
 
+typedef struct {
+  int cap;
+  Node** b;
+} HashMap;
 
-struct Node{
-    int key;
-    int value;
-};
-
-/*
-Me falto la cantidad de nodos a la que puede llegar, pero creo qeu no lo necesito
-
-en el ejmplo usan
-struct node** arr;
-pero aja, yo creo qeu lo puedo sustituir simplemente apuntando al primer nodo y ya
-
-mmm, he seguido el reto y a la hora de llenar el hashmap, me doy cuenta de qeu esta mas comodo si lo hago con
-struct node** arr;
-por que ahora depende del array, que es basicamente, donde se guardan los nodos
-*/
-struct HashMap{
-    int amountOfNodes;
-    struct Node** arr;
-};
-
-
-struct Node *createNode(int key, int value){
-    struct Node *node = malloc(sizeof(struct Node));
-    node->key = key;
-    node->value = value;
-    return node;
-
+static uint32_t h32(int k){
+  uint32_t x = (uint32_t)k;
+  x ^= x >> 16; x *= 0x7feb352dU;
+  x ^= x >> 15; x *= 0x846ca68bU;
+  x ^= x >> 16;
+  return x;
 }
 
-struct HashMap *createHashMap(int amountOfNodes){
-    
-    amountOfNodes+=1;
-    struct HashMap *hashMap = malloc (sizeof(struct HashMap));
-    hashMap->amountOfNodes = amountOfNodes;
-    hashMap->arr = malloc(amountOfNodes * sizeof(struct Node*));
-    
-    return hashMap;
+
+HashMap *hm_new(int cap){
+    HashMap *hm = malloc(sizeof(HashMap));
+    hm->cap = cap;
+    hm->b = malloc(cap * sizeof(Node*));
+    if(hm->b == NULL){
+        return 0;
+    }
+    return hm;
 }
 
+
+int hm_get(HashMap* hm, int key){
+    int bucketIdx = h32(key) % hm->cap;
+    Node* p = hm->b[bucketIdx];
+    while (p != NULL){
+        if(p->key == key){
+            return p->val;
+        } else{
+            p = p->next;
+        }
+    }
+    return 0;
+}
+
+
+void hm_inc(HashMap* hm, int key, int d){
+    int idx = h32(key) % hm->cap;
+    Node* p = hm->b[idx];
+    while (p != NULL){
+        if(p->key == key){
+            p->val += d;
+            return;
+        } else{   
+            p = p->next;
+        }
+    }
+    Node *newNode = malloc(d * sizeof(Node*));
+    newNode->key = key;
+    newNode->val = d;
+
+    
+    /*
+    al inicio, tenemos esto>
+    m->b[3] -> A -> B -> NULL
+    pero con
+    newNode->next = hm->b[idx];   
+    n -> A -> B -> NULL
+    luego con
+    hm->b[idx] = newNode; 
+    m->b[3] -> n -> A -> B -> NULL
+
+    */
+    newNode->next = hm->b[idx];   
+    hm->b[idx] = newNode; 
+    return;
+}
+
+
+void hm_free(HashMap* m){
+    if (!m) return;
+
+    for (int i = 0; i < m->cap; i++){
+        Node* p = m->b[i];
+        while (p){
+            Node* next = p->next;  
+            free(p);
+            p = next;
+        }
+    }
+    free(m->b);  
+    free(m);     
+}
 
 
 void subarraySum(int* nums, int numsSize, int k) {
 
+    int count = 0;
+    int cap = numsSize+1;
+    HashMap *hm = hm_new(cap);
+    
+    
     int *prefixes = malloc(numsSize * sizeof(int));
     int acumulador = 0;
-    int prefix = 0;
+    int minusK = 0;
+    hm_inc(hm, 0, 1);
+    for(int i = 0; i < numsSize; i++){
+        acumulador = acumulador + *(nums+i);  
+        *(prefixes+i) = acumulador;
+        printf("%d\n", *(prefixes+i));
+        int consulta = acumulador-k;
 
-    // de seguro hay algo de lo shash maps que hace que no sea tanto problema trabajar con ellos caundo se repiten cosas y asi, pero no es el foco, enotrnces por eso me saqeu esto de los huevos 
-    // de modo que caundo hay un cero, no se repita, el unico que cero qeu va a haber sera simplemente el inicial, el qeu esta de cajon. y como incluso caundo hay un cero en los prefijos, poner el cero en uno desde el inicio se tiene que hacer si o si
-     
-    struct HashMap *hashMap = createHashMap(numsSize); 
-    *((hashMap->arr)+0) = createNode(0, 1);
-    for (int i = 0; i < numsSize; i++){
-        prefix = nums[i] + acumulador;
-        prefixes[i] = prefix;
-        if (prefix == 0){
-        }else {
-            int inserccionArrayDelHashMap = i+1;
-            *((hashMap->arr)+inserccionArrayDelHashMap) = createNode(prefix, 0);
+        minusK = hm_get(hm, consulta);
+        if(minusK > 0){
+            count = count + minusK;
         }
-        acumulador += nums[i];
+        hm_inc(hm, acumulador, 1);
     }
 
-
-    for (int i = 0; i < numsSize+1; i++){
-        printf("%d", (*((hashMap->arr)+i))->key);
-    }
-
-
+    printf("\ncount: %d\n", count);
 
 }
-
-
-
-
-
 
 int main () {
     int largo = 6;
