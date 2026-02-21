49c75af7ab2758145616c9149933cc6d932507fa
Samuel Marquez <sammarquez833@outlook.com>
2026-02-21 15:01:11 -0600

    done koko bannanas

--- leetcodes/traversal_patterns/koko_bannanas/code.c
+++ leetcodes/traversal_patterns/koko_bannanas/code.c
@@ -13,37 +13,50 @@ int minEatingSpeed(int* piles, int pilesSize, int h) {
         }
     }
 
-    //printf("right: %d\n", right);
     
 
-    //chat dice que asi es como funciona...
-    // int kActualOelMedio = left + (right - left) / 2;
     int kActualOelMedio = (int)ceil((double)right / 2);
-    //printf("kActualOelMedio: %d\n", kActualOelMedio);
     int horasQueHanDeSerMaximoOcho = 0;
+    int ans = 0;
     
+
+    // dejamos la condicion como left <= right  
+    // por que caundo left == right significa solo queda un candidato posible en el rango
+    // entonces igual se tiene qeu evaluar
+    // por mi linea de pensamiento, la solucion se encunetra una vez se cruzan los indices
     while (left <= right){
+
+        printf("left: %d\nright: %d\n\n", left, right);
+
         for (int i = 0; i < pilesSize; i++){
             printf("n1: %d\nn2: %d\ny por ende lo que le sumamos a horasAcumuladas: %d\n", (*(piles+i)), kActualOelMedio, (int)ceil((double)(*(piles+i)) / (double)kActualOelMedio));
             horasQueHanDeSerMaximoOcho = horasQueHanDeSerMaximoOcho + (int)ceil((double)(*(piles+i)) / (double)kActualOelMedio);       
-            printf("terminamos el ciclo y la suma dio: %d\n", horasQueHanDeSerMaximoOcho);
         }  
+        printf("terminamos el ciclo y la suma dio: %d\n\n", horasQueHanDeSerMaximoOcho);
         
         
-        if(horasQueHanDeSerMaximoOcho == h){
-            printf("\n\n\nsu puta madre alv imprime esto me lleva a la verga\n\n\n");
-            printf("\n\nencontramos el numero output que seria: %d", left);
-            return 0;
-        } else if (horasQueHanDeSerMaximoOcho < h){
+        // y es qeu esa es la cosa
+        // linea de pensamiento original> lo voy haceindo y si un medio es igual a eso, pues devuelvo el medio, se qeu no queria hacer eso, pero es lo que estoy haciendo
+        // linea de pensamiento nueva> los indices se han de cruzar, y ahi, left trae la respuesta
+        // no puedes detenerte cuando son iguales por que ouede haber uno a la izquierda que es menor peor igual cumple 
+        if(horasQueHanDeSerMaximoOcho <= h){
             right = kActualOelMedio-1;
         } else if (horasQueHanDeSerMaximoOcho > h){
             left = kActualOelMedio+1;
         }
+        ans = left;
+
         kActualOelMedio = left + (int)floor((right-left)/2);
         horasQueHanDeSerMaximoOcho = 0;
-        printf("\n\n\n\n\n\n");
+        printf("\n\n");
     }
-    return 0;
+
+    printf("left: %d\nright: %d\n\n", left, right);
+
+
+    printf("output: %d\n", ans);
+    return ans;
+    
 
 }
 
@@ -51,9 +64,10 @@ int minEatingSpeed(int* piles, int pilesSize, int h) {
 
 
 int main() {
-    int pilesSize = 5;
-    int piles[] = {30,11,23,4,20};
-    int h = 6;
+    int pilesSize = 1;
+    int piles[] = {312884470};
+    int h = 312884469;
+
     minEatingSpeed(piles, pilesSize, h);
     return 0;
 }
