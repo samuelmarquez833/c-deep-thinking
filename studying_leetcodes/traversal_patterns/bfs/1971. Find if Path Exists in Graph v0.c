726217f1b008e92fc81c6f1446361853dfcadb86
Samuel Marquez <sammarquez833@outlook.com>
2026-03-17 20:15:25 -0600

    done with bfs

--- studying_leetcodes/traversal_patterns/bfs/1971. Find if Path Exists in Graph v0.c
+++ studying_leetcodes/traversal_patterns/bfs/1971. Find if Path Exists in Graph v0.c
@@ -5,44 +5,52 @@
 
 struct Queue{
     int front;
-    int back
+    int back;
     int* list;
-}
+};
 
-struct Queue* initQueueWithAValue(int size){
-    struct Queue* queue = malloc(sizeof(Queue));
-    queue->frot = -1;
-    queue->back= -1;
+struct Queue* initQueue(int size){
+    struct Queue* queue = malloc(sizeof(struct Queue));
+    queue->front = 0;
+    queue->back= 0;
+
     queue->list = malloc(size * sizeof(int));
     return queue;
 }
 
 
+/*
+sacas de front - haces crecer solo front 
+metes en back - haces crecer solo back
+
+*/
+
+
+
 // Put an elemento at the back
 void enqueue (struct Queue* queue, int n){
-    // if its empty, initialize back and front
-    if(queue->front == -1){
-        queue->list[queue->front] = -1;
-        queue->list[queue->back]= -1;
-        return
-    }
-    // it its not empty just set the back to equal that element 
-    queue->list[queue->back] = n;
+    queue->list[queue->back]= n;
+    queue->back += 1;        
 }
 
 // Get out the element of the front
-void dequeue (struct Queue* queue, int n){
-    // if there is only one element
-    if (queue->list[queue->front] == queue->list[queue->back]){
-
+int dequeue (struct Queue* queue){
+    if (queue->front == queue->back){
+        return -1;
     }
+    int out = queue->list[queue->front];        
+    queue->front += 1;        
+    return out;    
 }
+
+
 
 
 /*
 Build adjacency list so neighbors can be accessed in O(degree)
 instead of scanning the edges array each time.
 */
+
 void buildAdj(int nodeCount, int** edges, int edgeCount, int*** adj, int** adjSize){
     *adj = malloc(nodeCount * sizeof(int*));
     *adjSize = calloc(nodeCount, sizeof(int));
@@ -72,7 +80,13 @@ void buildAdj(int nodeCount, int** edges, int edgeCount, int*** adj, int** adjSi
 
 
 
-bool validPath1(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination){
+
+
+
+
+// I am implementing enqueue on pop since it is more careful at the time of filling the queue (its less probable of getting duplicates there)
+
+bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination){
     int** adj;
     int* adjSize;
     buildAdj(n, edges, edgesSize, &adj, &adjSize);
@@ -80,13 +94,42 @@ bool validPath1(int n, int** edges, int edgesSize, int* edgesColSize, int source
     int visited[n];
     for(int i = 0; i < n; i++) visited[i] = 0;
 
+    struct Queue* queue = initQueue(n); 
+    enqueue(queue, source);
+    visited[source] = 1;
+
     int currentNode;
+    
 
-    while(stack->top != -1){
+    
+    while(queue->front != queue->back){
 
+        currentNode = dequeue(queue);
+        printf("curr: %d\n", currentNode);
+        printf("front: %d\n", queue->front);
+        printf("back: %d\n", queue->back);
+        
+        if (currentNode == destination){
+            printf("We have found a valid way from source to destination");
+            return true;
+        }
 
+        for(int i = 0; i < adjSize[currentNode]; i++){
+            int neighbor = adj[currentNode][i];
+            if (visited[neighbor] == 0){
+                printf("aniadimos un vecino: %d\n", neighbor);
+                visited[neighbor] = 1;
+                enqueue(queue, neighbor);
+            }
+        } 
+    
     }
     
+    printf("We have not found a valid way from source to destination");
+    return false;
+
+
+
 }
 
 
@@ -110,5 +153,5 @@ int main(){
     int source = 0;
     int destination = 3;
 
-    bool out = validPath2(nodeCount, edges, edgeCount, &edgesColSize, source, destination);
+    bool out = validPath(nodeCount, edges, edgeCount, &edgesColSize, source, destination);
 }
