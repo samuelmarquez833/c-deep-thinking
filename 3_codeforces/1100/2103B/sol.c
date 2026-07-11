eb36b541ac4aad45d765c2eb34d3eb94c747dc12
Samuel Marquez <sammarquez833@outlook.com>
2026-07-11 16:46:59 -0600

    2103B

---
+++ 3_codeforces/1100/2103B/sol.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void funcion (int s, char *string){
+
+    int cuentaDeCambios = 0;
+    if (string[0] == '1') {
+        cuentaDeCambios++;
+    }
+
+    for (int i = 1; i < s; i++){
+        if (string[i] != string[i-1]){
+            cuentaDeCambios++;
+        }
+    }
+
+    if (cuentaDeCambios >= 3){
+        cuentaDeCambios -= 2;
+    }
+
+    if (cuentaDeCambios == 2){
+        cuentaDeCambios -= 1;
+        
+    }
+
+    int costo = s + cuentaDeCambios;
+
+    printf("%d\n", costo);
+    
+}
+
+
+/*
+int main (){
+
+    int n = 3;
+    int string[3] = {0,0,0};  
+    funcion(n, string);
+
+    return 0;
+}
+*/
+
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        char *string = malloc((n) * sizeof(char));
+        scanf("%s", string);
+        
+        funcion(n, string);
+        free(string);
+    }
+}
